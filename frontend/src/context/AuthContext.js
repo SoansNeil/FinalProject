62fd4910b8f00c736cea84a91b5cac3f50bf7f15
Neil Soans <nellysoans8@gmail.com>
2025-11-24 19:07:41 -0600

    changed authContext to export useAuth

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,7 +1,9 @@
 import React, { createContext, useState, useCallback, useEffect } from 'react';
-
+import { useContext } from "react";
 export const AuthContext = createContext();
-
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
